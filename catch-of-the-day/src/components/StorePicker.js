e63d215d52c20d53e37d1dd64f6b552d24c77f06
Miriam Hernandez <miriamhz14@gmail.com>
2016-12-30 15:49:27 -0500

    lesson 12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,26 +6,29 @@ class StorePicker extends React.Component {
   //   super();
   //   this.goToStore = this.goToStore.bind(this);
   // }
-
-  goToStore(event){
+  goToStore(event) {
     event.preventDefault();
     console.log('You Changed the URL');
-    // first grab text from box
-    console.log(this.storeInput.value);
-    // second we're going to transition from / to / store/ :storeId
+    // first grab  text from  box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`)
+    // second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
-
 
   render() {
     return (
-        <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
-          <h2> Please Enter A Store </h2>
-          <input type="text" required placeholder="Store Name"
-            defaultValue={getFunName()} ref={(input) => { this.storeInput }} />
-          <button type="submit"> Visit Store →</button>
-        </form>
-      )
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
