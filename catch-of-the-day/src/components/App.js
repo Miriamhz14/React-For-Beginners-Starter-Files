ce0779bfa2870a61cf577b1806158493718147bc
Miriam Hernandez <miriamhz14@gmail.com>
2016-12-30 18:23:27 -0500

    lesson 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,38 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
+  constructor(){
+  super();
+
+  this.addFish= this.addFish.bind(this);
+  this.loadSamples = this.loadSamples.bind(this);
+    // getinitial state
+  this.state = {
+  fishes: {},
+  order: {}
+  };
+}
+
+  addFish(fish){
+   //update state
+   const fishes = {...this.state.fishes};
+   //addin new fish
+   const timestamp = Date.now();
+   fishes['fish-${timestamp}'] = fish;
+   // this.state.fishes.fish1 = fish;
+   //set state
+   this.setState({ fishes });
+}
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+  });
+}
+
   render() {
     return (
         <div className="catch-of-the-day">
@@ -11,7 +41,7 @@ class App extends React.Component {
               <Header tagline="Fresh Seafood Market"/>
             </div>
            <Order />
-           <Inventory />
+           <Inventory addFish={this.addFish} />
       </div>
       )
   }
